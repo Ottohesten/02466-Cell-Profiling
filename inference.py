084ef6986e4b63405d0d1e8544c6355a2d5eef13
Ottohesten <otto@skytop.dk>
2024-03-31 23:09:55 +0200

    generalizing inference to work with own dataset

--- inference.py
+++ inference.py
@@ -9,7 +9,7 @@ class Inference:
         self.model.to('cpu')
         self.num_samples = num_samples
         self.test_loader = test_loader
-        self.digits = self.generate_digits(num_samples)
+        self.samples = self.generate_samples(num_samples)
 
     # def __call__(self, x):
     #     if cuda:
@@ -18,18 +18,18 @@ class Inference:
     #         x_hat, mu, logvar = self.model(x)
     #     return x_hat, mu, logvar
         
-    def generate_digits(self, num_samples):
-        digits = defaultdict(torch.Tensor)
+    def generate_samples(self, num_samples):
+        samples = defaultdict(torch.Tensor)
         with torch.no_grad():
             for batch_x, batch_y in self.test_loader:
                 # if cuda:
                 #     batch_x = batch_x.cuda()
                 x_hat, mu, sigma = self.model(batch_x)
                 for x, y in zip(x_hat, batch_y):
-                    if len(digits[y.item()]) < num_samples:
+                    if len(samples[str(y.item())]) < num_samples:
                         # add the generated image to the list
-                        digits[y.item()] = torch.cat((digits[y.item()], x.unsqueeze(0)), dim=0)
+                        samples[str(y.item())] = torch.cat((samples[str(y.item())], x.unsqueeze(0)), dim=0)
                     else:
                         continue
                     
-        return digits
+        return samples
